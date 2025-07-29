327ab35da755884c5b4e1763a10b3ca15af70b3f
Akash <akash.krishnan.046@gmail.com>
2025-07-29 21:31:07 +0530

    feat: add meteor animation and refactor orbiting circles demo layout

--- components/O.tsx
+++ components/O.tsx
@@ -9,25 +9,37 @@ import NextIcon from "@/assets/next.svg";
 export function OrbitingCirclesDemo() {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
+      {/* Falling particles background */}
+       <div className="relative overflow-hidden h-[500px] w-full max-w-[350px]">
+  
+</div>
+
+      {/* Center Text */}
+      <div className="absolute z-10 text-white text-xl font-bold animate-pulse pointer-events-none">
+        Stack
+      </div>
+
+      {/* Orbits */}
       <OrbitingCircles iconSize={40}>
-   <Icons.react />
-  <Icons.gitHub />
-  <Icons.python />
-  <Icons.next />
-</OrbitingCircles>
-<OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
-   <Icons.react />
-  <Icons.gitHub />
-  <Icons.python />
-  <Icons.next />
-</OrbitingCircles>
+        {Icons.react()}
+        {Icons.gitHub()}
+        {Icons.python()}
+        {Icons.next()}
+      </OrbitingCircles>
+
+      <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
+        {Icons.react()}
+        {Icons.gitHub()}
+        {Icons.python()}
+        {Icons.next()}
+      </OrbitingCircles>
     </div>
   );
 }
 
 const Icons = {
-  react: () => <ReIcon className="w-10 h-10" />,
-  gitHub: () => <GitIcon className="w-10 h-10" />,
-  python: () => <PythonIcon className="w-10 h-10" />,
-  next: () => <NextIcon className="w-10 h-10" />,
+  react: () => <ReIcon className="w-10 h-10 hover:scale-110 transition-transform duration-300" />,
+  gitHub: () => <GitIcon className="w-10 h-10 hover:scale-110 transition-transform duration-300" />,
+  python: () => <PythonIcon className="w-10 h-10 hover:scale-110 transition-transform duration-300" />,
+  next: () => <NextIcon className="w-10 h-10 hover:scale-110 transition-transform duration-300" />,
 };
